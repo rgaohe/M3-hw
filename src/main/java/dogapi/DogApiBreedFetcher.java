fca8b3a2d224a44b0d4c9f7af40494256897d4a5
russell <russell.gao@mail.utoronto.ca>
2025-10-17 23:25:18 -0400

    second”

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -12,7 +12,7 @@ import java.util.*;
 /**
  * BreedFetcher implementation that relies on the dog.ceo API.
  * Note that all failures get reported as BreedNotFoundException
- * exceptions to align with the requirements of the BreedFetcher interface.
+ * exceptions to align with the requirements of the dBreedFetcher interface.
  */
 public class DogApiBreedFetcher implements BreedFetcher {
     private final OkHttpClient client = new OkHttpClient();
